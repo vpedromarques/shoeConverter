5506c1fd5d2f03ec1687334931fa99422cf8c0ab
Pedro Marques <mail@pedromarques.com>
2015-03-24 12:12:54 +0000

    Shoe Converter

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mensShoeSizeTextfield: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
 
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+//        let sizeFromTextField = mensShoeSizeTextfield.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+        
+        let sizeFromTextField = mensShoeSizeTextfield.text.toInt()!
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        
+        
+        // clean inserted value and remove keyboard
+        mensShoeSizeTextfield.text = ""
+        mensShoeSizeTextfield.resignFirstResponder()
+        
+    }
 
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton) {
+        
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        
+        // clean inserted value and remove keyboard
+        womensShoeSizeTextField.text = ""
+       womensShoeSizeTextField.resignFirstResponder()
+        
+        
+        
+    }
 }
 
